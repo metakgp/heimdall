ec9472064d0c8b959261093fb0aef1ec758bad67
Chirag Ghosh <cghosh828049@gmail.com>
2023-09-17 05:40:25 +0530

    feat: initial working code;

---
+++ go.mod
@@ -0,0 +1,15 @@
+module example.com/m
+
+go 1.21.1
+
+require github.com/likexian/whois v1.15.1
+
+require (
+	github.com/likexian/gokit v0.25.13 // indirect
+	golang.org/x/text v0.12.0 // indirect
+)
+
+require (
+	github.com/rs/cors v1.10.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"regexp"
+
+	"github.com/likexian/whois"
+	"github.com/rs/cors"
+)
+
+func handleRequest(res http.ResponseWriter, req *http.Request) {
+	clientIP := req.Header.Get("X-Forwarded-For")
+	whoisResponse, err := whois.Whois(clientIP)
+	if err != nil {
+		fmt.Println(err)
+	}
+	
+	// Define a regular expression pattern to match the netname
+    pattern := `netname:\s+(.*)`
+
+    // Compile the regular expression
+    re := regexp.MustCompile(pattern)
+
+    // Find the netname using the regular expression
+    match := re.FindStringSubmatch(whoisResponse)
+
+	response := make(map[string]bool)
+
+    if len(match) >= 2 {
+        netname := match[1]
+        if netname == "IITKGP-IN" {
+			response["is_inside_kgp"] = true
+		} else {
+			response["is_inside_kgp"] = false
+		}
+    } else {
+        fmt.Println("Netname not found in the whois response.")
+		response["is_inside_kgp"] = false
+    }
+
+	res.Header().Set("Content-Type", "application/json")
+	res.Header().Set("Access-Control-Allow-Origin", "*")
+	jsonResp, err := json.Marshal(response)
+	if err != nil {
+		fmt.Println(err)
+	}
+	res.Write(jsonResp)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handleRequest)
+
+	handler := cors.AllowAll().Handler(mux)
+	err := http.ListenAndServe(":3333", handler)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
+}
