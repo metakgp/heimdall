80755d7c1943d9f77d6d050a29aac23cfa16610f
Chirag Ghosh <cghosh828049@gmail.com>
2024-06-15 15:29:02 +0530

    feat: move campus check endpoint from / to /campus_check
    Signed-off-by: Chirag Ghosh <cghosh828049@gmail.com>

--- main.go
+++ main.go
@@ -12,35 +12,35 @@ import (
 	"github.com/rs/cors"
 )
 
-func handleRequest(res http.ResponseWriter, req *http.Request) {
+func handleCampusCheck(res http.ResponseWriter, req *http.Request) {
 	clientIP := req.Header.Get("X-Forwarded-For")
 	whoisResponse, err := whois.Whois(clientIP)
 	if err != nil {
 		fmt.Println(err)
 	}
-	
+
 	// Define a regular expression pattern to match the netname
-    pattern := `netname:\s+(.*)`
+	pattern := `netname:\s+(.*)`
 
-    // Compile the regular expression
-    re := regexp.MustCompile(pattern)
+	// Compile the regular expression
+	re := regexp.MustCompile(pattern)
 
-    // Find the netname using the regular expression
-    match := re.FindStringSubmatch(whoisResponse)
+	// Find the netname using the regular expression
+	match := re.FindStringSubmatch(whoisResponse)
 
 	response := make(map[string]bool)
 
-    if len(match) >= 2 {
-        netname := match[1]
-        if netname == "IITKGP-IN" {
+	if len(match) >= 2 {
+		netname := match[1]
+		if netname == "IITKGP-IN" {
 			response["is_inside_kgp"] = true
 		} else {
 			response["is_inside_kgp"] = false
 		}
-    } else {
-        fmt.Println("Netname not found in the whois response.")
+	} else {
+		fmt.Println("Netname not found in the whois response.")
 		response["is_inside_kgp"] = false
-    }
+	}
 
 	res.Header().Set("Content-Type", "application/json")
 	res.Header().Set("Access-Control-Allow-Origin", "*")
@@ -53,7 +53,7 @@ func handleRequest(res http.ResponseWriter, req *http.Request) {
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", handleRequest)
+	mux.HandleFunc("/campus_check", handleCampusCheck)
 
 	handler := cors.AllowAll().Handler(mux)
 	err := http.ListenAndServe(":3333", handler)
